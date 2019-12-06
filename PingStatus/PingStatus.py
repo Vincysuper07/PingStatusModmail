b542a585488b72dc84f5a01ece2a46d6c96a118d
Vincysuper07 <52707876+Vincysuper07@users.noreply.github.com>
2019-12-06 21:40:30 +0100

    Update PingStatus.py

--- PingStatus/PingStatus.py
+++ PingStatus/PingStatus.py
@@ -1,1 +1,60 @@
+import discord
+from discord.enums import ActivityType, Status
+from discord.ext import commands
 
+    @commands.command(aliases=["presence"])
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def activity(self, ctx, activity_type: str.lower, *, message: str = ""):
+        """
+        Set an activity status for the bot.
+
+        Possible activity types:
+            - `playing`
+            - `streaming`
+            - `listening`
+            - `watching`
+
+        When activity type is set to `listening`,
+        it must be followed by a "to": "listening to..."
+
+        When activity type is set to `streaming`, you can set
+        the linked twitch page:
+        - `{prefix}config set twitch_url https://www.twitch.tv/somechannel/`
+
+        To remove the current activity status:
+        - `{prefix}activity clear`
+        """
+        if activity_type == "clear":
+            self.bot.config.remove("activity_type")
+            self.bot.config.remove("activity_message")
+            await self.bot.config.update()
+            await self.set_presence()
+            embed = discord.Embed(title="Activity Removed", color=self.bot.main_color)
+            return await ctx.send(embed=embed)
+
+        if not message:
+            raise commands.MissingRequiredArgument(SimpleNamespace(name="message"))
+
+        try:
+            activity_type = ActivityType[activity_type]
+        except KeyError:
+            raise commands.MissingRequiredArgument(SimpleNamespace(name="activity"))
+
+        activity, _ = await self.set_presence(
+            activity_type=activity_type, activity_message=message
+        )
+
+        self.bot.config["activity_type"] = activity.type.value
+        self.bot.config["activity_message"] = activity.name
+        await self.bot.config.update()
+
+        msg = f"Activity set to: {activity.type.name.capitalize()} "
+        if activity.type == ActivityType.listening:
+            msg += f"to {activity.name}."
+        else:
+            msg += f"{activity.name}."
+
+        embed = discord.Embed(
+            title="Activity Changed", description=msg, color=self.bot.main_color
+        )
+        return await ctx.send(embed=embed)
