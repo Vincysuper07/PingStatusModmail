dd50a9fdd132f3f63b003e2dc1f5c76a9c935ad2
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-12-11 22:00:45 +0300

    oop

--- PingStatus/PingStatus.py
+++ PingStatus/PingStatus.py
@@ -4,7 +4,7 @@ from discord.ext import commands, tasks
 
 class PingActivity(commands.Cog):
       
-      def __init__(self, bot):
+    def __init__(self, bot):
             self.bot = bot
             
     async def _set_presence(self):
